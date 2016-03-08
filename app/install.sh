6bda9a1174d5cdb097d810e6bf5faa5fccf3fd33
Blaine Schanfeldt <git@blaines.me>
2016-03-08 12:05:33 -0800

    Created install.sh to quickly make and install
    Will install applications to /usr/local/bin

---
+++ app/install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# make and install all cui applications by gcc to /usr/local/bin
+#
+
+echo; echo % pos2kml/gcc
+cd pos2kml/gcc
+make clean $1 && \
+make $1 && \
+make install $1
+cd ../..
+
+echo; echo % str2str/gcc
+cd str2str/gcc
+make clean $1 && \
+make $1 && \
+make install $1
+cd ../..
+
+echo; echo % rnx2rtkp/gcc
+cd rnx2rtkp/gcc
+make clean $1 && \
+make $1 && \
+make install $1
+cd ../..
+
+echo; echo % convbin/gcc
+cd convbin/gcc
+make clean $1 && \
+make $1 && \
+make install $1
+cd ../..
+
+echo; echo % rtkrcv/gcc
+cd rtkrcv/gcc
+make clean $1 && \
+make $1 && \
+make install $1
+cd ../..
