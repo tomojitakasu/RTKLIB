1a31154b3c34d232c6a250a539bc35c421af5e59
Tomoji Takasu <ttaka@yk.rim.or.jp>
2016-05-10 11:34:10 +0900

    rtklib 2.4.3 b9

--- src/rcvraw.c
+++ src/rcvraw.c
@@ -88,45 +88,46 @@ static double getbitg(const unsigned char *buff, int pos, int len)
     return getbitu(buff,pos,1)?-value:value;
 }
 /* decode Galileo I/NAV ephemeris ----------------------------------------------
-* decode Galileo I/NAV (ref [4] 4.3)
-* args   : unsigned char *buff I galileo I/NAV subframe bits (128 x 6 bits)
-*                                  buff[ 0- 15]: I/NAV pages word 0
-*                                  buff[16- 31]: I/NAV pages word 1
-*                                  buff[32- 47]: I/NAV pages word 2
-*                                  buff[48- 63]: I/NAV pages word 3
-*                                  buff[64- 79]: I/NAV pages word 4
-*                                  buff[80- 95]: I/NAV pages word 5
+* decode Galileo I/NAV (ref [5] 4.3)
+* args   : unsigned char *buff I Galileo I/NAV subframe bits
+*                                  buff[ 0-15]: I/NAV word type 0 (128 bit)
+*                                  buff[16-31]: I/NAV word type 1
+*                                  buff[32-47]: I/NAV word type 2
+*                                  buff[48-63]: I/NAV word type 3
+*                                  buff[64-79]: I/NAV word type 4
+*                                  buff[80-95]: I/NAV word type 5
 *          eph_t    *eph    IO  ephemeris structure
 * return : status (1:ok,0:error)
 *-----------------------------------------------------------------------------*/
 extern int decode_gal_inav(const unsigned char *buff, eph_t *eph)
 {
-    gtime_t time1,time2;
     double tow,toc,tt,sqrtA;
-    unsigned int time_flag,week,svid,e5b_hs,e1b_hs,e5b_dvs,e1b_dvs,iod_nav[4];
-    int i;
+    int i,time_f,week,svid,e5b_hs,e1b_hs,e5b_dvs,e1b_dvs,type[6],iod_nav[4];
     
-    i=6; /* word 0 */
-    time_flag  =getbitu(buff,i, 2);              i+= 2+88;
+    i=0; /* word type 0 */
+    type[0]    =getbitu(buff,i, 6);              i+= 6;
+    time_f     =getbitu(buff,i, 2);              i+= 2+88;
     week       =getbitu(buff,i,12);              i+=12;
     tow        =getbitu(buff,i,20);
     
-    i=128+6; /* word 1 */
+    i=128; /* word type 1 */
+    type[1]    =getbitu(buff,i, 6);              i+= 6;
     iod_nav[0] =getbitu(buff,i,10);              i+=10;
     eph->toes  =getbitu(buff,i,14)*60.0;         i+=14;
     eph->M0    =getbits(buff,i,32)*P2_31*SC2RAD; i+=32;
     eph->e     =getbitu(buff,i,32)*P2_33;        i+=32;
     sqrtA      =getbitu(buff,i,32)*P2_19;
-    eph->A     =sqrtA*sqrtA;
     
-    i=256+6; /* word 2 */
+    i=128*2; /* word type 2 */
+    type[2]    =getbitu(buff,i, 6);              i+= 6;
     iod_nav[1] =getbitu(buff,i,10);              i+=10;
     eph->OMG0  =getbits(buff,i,32)*P2_31*SC2RAD; i+=32;
     eph->i0    =getbits(buff,i,32)*P2_31*SC2RAD; i+=32;
     eph->omg   =getbits(buff,i,32)*P2_31*SC2RAD; i+=32;
     eph->idot  =getbits(buff,i,14)*P2_43*SC2RAD;
     
-    i=384+6; /* word 3 */
+    i=128*3; /* word type 3 */
+    type[3]    =getbitu(buff,i, 6);              i+= 6;
     iod_nav[2] =getbitu(buff,i,10);              i+=10;
     eph->OMGd  =getbits(buff,i,24)*P2_43*SC2RAD; i+=24;
     eph->deln  =getbits(buff,i,16)*P2_43*SC2RAD; i+=16;
@@ -136,7 +137,8 @@ extern int decode_gal_inav(const unsigned char *buff, eph_t *eph)
     eph->crs   =getbits(buff,i,16)*P2_5;         i+=16;
     eph->sva   =getbitu(buff,i, 8);
     
-    i=512+6; /* word 4 */
+    i=128*4; /* word type 4 */
+    type[4]    =getbitu(buff,i, 6);              i+= 6;
     iod_nav[3] =getbitu(buff,i,10);              i+=10;
     svid       =getbitu(buff,i, 6);              i+= 6;
     eph->cic   =getbits(buff,i,16)*P2_29;        i+=16;
@@ -146,14 +148,26 @@ extern int decode_gal_inav(const unsigned char *buff, eph_t *eph)
     eph->f1    =getbits(buff,i,21)*P2_46;        i+=21;
     eph->f2    =getbits(buff,i, 6)*P2_59;
     
-    i=640+6+41; /* word 5 */
+    i=128*5; /* word type 5 */
+    type[5]    =getbitu(buff,i, 6);              i+= 6+41;
     eph->tgd[0]=getbits(buff,i,10)*P2_32;        i+=10; /* BGD E5a/E1 */
     eph->tgd[1]=getbits(buff,i,10)*P2_32;        i+=10; /* BGD E5b/E1 */
-    e5b_hs     =getbitu(buff,i, 2);              i+= 2; /* E5B_hs */
-    e1b_hs     =getbitu(buff,i, 2);              i+= 2; /* E1B_hs */
-    e5b_dvs    =getbitu(buff,i, 1);              i+= 1; /* E5B_dvs */
-    e1b_dvs    =getbitu(buff,i, 1);                     /* E1B_dvs */
+    e5b_hs     =getbitu(buff,i, 2);              i+= 2;
+    e1b_hs     =getbitu(buff,i, 2);              i+= 2;
+    e5b_dvs    =getbitu(buff,i, 1);              i+= 1;
+    e1b_dvs    =getbitu(buff,i, 1);
     
+    /* test word types */
+    if (type[0]!=0||type[1]!=1||type[2]!=2||type[3]!=3||type[4]!=4) {
+        trace(3,"decode_gal_inav error: type=%d %d %d %d %d\n",type[0],type[1],
+              type[2],type[3],type[4]);
+        return 0;
+    }
+    /* test word type 0 time field */
+    if (time_f!=2) {
+        trace(3,"decode_gal_inav error: word0-time=%d\n",time_f);
+        return 0;
+    }
     /* test consistency of iod_nav */
     if (iod_nav[0]!=iod_nav[1]||iod_nav[0]!=iod_nav[2]||iod_nav[0]!=iod_nav[3]) {
         trace(3,"decode_gal_inav error: ionav=%d %d %d %d\n",iod_nav[0],
@@ -164,22 +178,17 @@ extern int decode_gal_inav(const unsigned char *buff, eph_t *eph)
         trace(2,"decode_gal_inav svid error: svid=%d\n",svid);
         return 0;
     }
+    eph->A=sqrtA*sqrtA;
     eph->iode=eph->iodc=iod_nav[0];
-    eph->svh=(e5b_hs<<7)|(e5b_dvs<<6)|(e1b_hs<<2)|e1b_dvs;
-    
-    if (time_flag!=2) {
-        trace(2,"decode_gal_inav: gst week info not obtained\n");
-        return 0;
-    }
+    eph->svh=(e5b_hs<<7)|(e5b_dvs<<6)|(e1b_hs<<1)|e1b_dvs;
     eph->ttr=gst2time(week,tow);
-    tt=timediff(gst2time(week,eph->toes),eph->ttr);
+    tt=timediff(gst2time(week,eph->toes),eph->ttr); /* week complient to toe */
     if      (tt> 302400.0) week--;
     else if (tt<-302400.0) week++;
     eph->toe=gst2time(week,eph->toes);
     eph->toc=gst2time(week,toc);
-    
-    eph->week=week+1024; /* galileo-week = gst-week + 1024 */
-    eph->code=1;         /* data source = I/NAV E1-B */
+    eph->week=week+1024; /* gal-week = gst-week + 1024 */
+    eph->code=1;         /* data source = I/NAV E1B */
     
     return 1;
 }
