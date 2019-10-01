0bb93abb1d50189c5ac9451ef52b4b7bb4603da8
Sam Price <samuel.r.price@nasa.gov>
2019-10-01 15:20:46 -0400

    Add ifdef around B460800 for mac sport.

--- src/stream.c
+++ src/stream.c
@@ -350,7 +350,10 @@ static serial_t *openserial(const char *path, int mode, char *msg)
 #else
     const speed_t bs[]={
         B300,B600,B1200,B2400,B4800,B9600,B19200,B38400,B57600,B115200,B230400,
+
+        #ifdef B460800
         B460800,B921600
+        #endif
     };
     struct termios ios={0};
     int rw=0;
