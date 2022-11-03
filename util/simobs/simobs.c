0a05edec651cf949af0294d81e9b3330dfc760f7
Hauschild, André <andre.hauschild@dlr.de>
2022-11-03 14:51:04 +0100

    Add navigation system selection to command line options, remove obsolete variables

--- util/simobs/simobs.c
+++ util/simobs/simobs.c
@@ -39,11 +39,11 @@ static double randn(double myu, double sig)
 /* set string without tail space ---------------------------------------------*/
 static void setstr(char *dst, const char *src, int n)
 {
-    char *p=dst;
-    const char *q=src;
-    while (*q&&q<src+n) *p++=*q++;
-    *p--='\0';
-    while (p>=dst&&*p==' ') *p--='\0';
+  char *p=dst;
+  const char *q=src;
+  while (*q&&q<src+n) *p++=*q++;
+  *p--='\0';
+  while (p>=dst&&*p==' ') *p--='\0';
 }
 /* generate snr --------------------------------------------------------------*/
 static void snrmodel(const double *azel, double *snr)
@@ -79,39 +79,33 @@ static void errmodel(const double *azel, double *snr, double *ecp, double *epr)
 }
 /* generate simulated observation data ---------------------------------------*/
 static int simobs(gtime_t ts, gtime_t te, double tint, const double *rr,
-    nav_t *nav, obs_t *obs, int opt)
-{
-  gtime_t time;
-  obsd_t data[MAXSAT]={{{0}}};
-  double pos[3],rs[6*MAXSAT],dts[2*MAXSAT],r,e[3],azel[2];
-  double ecp[MAXSAT][NFREQ]={{0}},epr[MAXSAT][NFREQ]={{0}};
-  double snr[MAXSAT][NFREQ]={{0}},ers[MAXSAT][3]={{0}};
-  double iono,trop,fact,cp,pr,dtr=0.0,rref[3],bl,var[MAXSAT];
-  int i,j,k,n,ns,amb[MAXSAT][NFREQ]={{0}},sys,prn,svh[MAXSAT];
-  char s[64];
-  double f0,fk;
-  double pref[]={36.106114294,140.087190410,70.3010}; /* ref station */
+                  nav_t *nav, obs_t *obs) {
+
+  gtime_t   time;
+  obsd_t    data[MAXSAT]={{{0}}};
+  double    pos[3],rs[6*MAXSAT],dts[2*MAXSAT],r,e[3],azel[2];
+  double    ecp[MAXSAT][NFREQ]={{0}},epr[MAXSAT][NFREQ]={{0}};
+  double    snr[MAXSAT][NFREQ]={{0}},ers[MAXSAT][3]={{0}};
+  double    iono,trop,fact,cp,pr,dtr=0.0,var[MAXSAT];
+  int       i,j,k,n,ns,amb[MAXSAT][NFREQ]={{0}},sys,prn,svh[MAXSAT];
+  char      s[64];
+  double    f0,fk;
 
   trace(3,"simobs:nnav=%d ngnav=%d\n",nav->n,nav->ng);
-
-  for (i=0;i<2;i++) pref[i]*=D2R;
-  pos2ecef(pref,rref);
-  for (i=0;i<3;i++) rref[i]-=rr[i];
-  bl=norm(rref,3)/1E4; /* baseline (10km) */
-  srand(0);
 
   /* ephemeris error */
   for (i=0;i<MAXSAT;i++) {
     data[i].sat=i+1;
     data[i].P[0]=2E7;
     for (j=0;j<3;j++) ers[i][j]=randn(0.0,erreph);
-  }
+  };
   srand(tickget());
 
   ecef2pos(rr,pos);
   n=(int)(timediff(te,ts)/tint+1.0);
 
   for (i=0;i<n;i++) {
+
     time=timeadd(ts,tint*i);
     time2str(time,s,0);
 
@@ -122,8 +116,8 @@ static int simobs(gtime_t ts, gtime_t te, double tint, const double *rr,
       for (k=0;k<MAXSAT;k++) {
         if ((r=geodist(rs+k*6,rr,e))<=0.0) continue;
         data[k].P[0]=+CLIGHT*(dtr-dts[k*2]);
-      }
-    }
+      };
+    };
     satposs(time,data,MAXSAT,nav,EPHOPT_BRDC,rs,dts,var,svh);
     for (j=ns=0;j<MAXSAT;j++) {
 
@@ -134,11 +128,11 @@ static int simobs(gtime_t ts, gtime_t te, double tint, const double *rr,
       satazel(pos,e,azel);
       if (azel[1]<minel*D2R) continue;
 
+      /* Compute L1 ionospheric delay */
       iono=ionmodel(time,nav->ion_gps,pos,azel);
-      trop=tropmodel(time,pos,azel,0.3);
 
-      /* add ionospheric error */
-      iono+=errion*bl*ionmapf(pos,azel);
+      /* Compute tropospheric delay */
+      trop=tropmodel(time,pos,azel,0.3);
 
       snrmodel(azel,snr[j]);
       errmodel(azel,snr[j],ecp[j],epr[j]);
@@ -146,7 +140,8 @@ static int simobs(gtime_t ts, gtime_t te, double tint, const double *rr,
 
       for (k=0;k<NFREQ;k++) {
 
-        data[j].L[k]=data[j].P[k]=0.0;
+        data[j].L[k]=0.0;
+        data[j].P[k]=0.0;
         data[j].SNR[k]=0;
         data[j].LLI[k]=0;
         data[j].code[k]=CODE_NONE;
@@ -178,7 +173,9 @@ static int simobs(gtime_t ts, gtime_t te, double tint, const double *rr,
             default : continue;
           };
         }
-        else continue;
+        else {
+          continue;
+        };
 
         /* generate observation data */
         f0 = sat2freq(data[j].sat, CODE_L1C, nav);
@@ -192,43 +189,56 @@ static int simobs(gtime_t ts, gtime_t te, double tint, const double *rr,
         data[j].P[k]=pr;
         data[j].SNR[k]=(uint16_t)(snr[j][k]/SNR_UNIT+0.5);
         data[j].LLI[k]=data[j].SNR[k]<slipthres?1:0;
-      }
+
+      };
       if (obs->nmax<=obs->n) {
+
         if (obs->nmax==0) obs->nmax=65532; else obs->nmax+=65532;
         if (!(obs->data=(obsd_t *)realloc(obs->data,sizeof(obsd_t)*obs->nmax))) {
           fprintf(stderr,"malloc error\n");
           return 0;
-        }
-      }
+        };
+
+      };
+
       obs->data[obs->n++]=data[j];
       ns++;
-    }
+
+    };
     fprintf(stderr,"time=%s nsat=%2d\r",s,ns);
-  }
+  };
+
   fprintf(stderr,"\n");
   return 1;
-}
+
+};
+
 /* simobs main ---------------------------------------------------------------*/
-int main(int argc, char **argv)
-{
-  FILE *fp;
-  rnxopt_t rnxopt={{0}};
-  obs_t obs={0};
-  nav_t nav={0};
-  sta_t sta={0};
-  gtime_t ts={0},te={0};
-  double es[]={2000,1,1,0,0,0},ee[]={2000,1,1,0,0,0},tint=30.0;
-  double pos[3]={0},rr[3];
-  char *infile[16]={0},*outfile="";
-  int i,j,n=0,opt=0;
+int main(int argc, char **argv) {
+
+  FILE      *fp;
+  rnxopt_t  rnxopt={{0}};
+  obs_t     obs={0};
+  nav_t     nav={0};
+  sta_t     sta={0};
+  gtime_t   ts={0},te={0};
+  double    es[]={2000,1,1,0,0,0},ee[]={2000,1,1,0,0,0},tint=30.0;
+  double    pos[3]={0},rr[3];
+  char      *infile[16]={0},*outfile="";
+  int       i,j,n=0;
 
   traceopen("simobs.log");
   tracelevel(5);
 
+  /* Process command line arguments */
+
   for (i=1;i<argc;i++) {
 
-    if      (!strcmp(argv[i],"-o")&&i+1<argc) outfile=argv[++i];
+    if      (!strcmp(argv[i],"-o")&&i+1<argc) {
+      outfile=argv[++i];
+    }
     else if (!strcmp(argv[i],"-ts")) {
+
       if (i+2<argc && argv[i+2][0]!='-') {
         sscanf(argv[++i],"%lf/%lf/%lf",es,es+1,es+2);
         sscanf(argv[++i],"%lf:%lf:%lf",es+3,es+4,es+5);
@@ -241,8 +251,10 @@ int main(int argc, char **argv)
         return -1;
       };
       ts=epoch2time(es);
+
     }
     else if (!strcmp(argv[i],"-te")&&i+1<argc) {
+
       if (i+2<argc && argv[i+2][0]!='-') {
         sscanf(argv[++i],"%lf/%lf/%lf",ee,ee+1,ee+2);
         sscanf(argv[++i],"%lf:%lf:%lf",ee+3,ee+4,ee+5);
@@ -255,28 +267,58 @@ int main(int argc, char **argv)
         return -1;
       };
       te=epoch2time(ee);
+
     }
-    else if (!strcmp(argv[i],"-ti")&&i+1<argc) tint=atof(argv[++i]);
+    else if (!strcmp(argv[i],"-ti")&&i+1<argc) {
+      tint=atof(argv[++i]);
+    }
     else if (!strcmp(argv[i],"-r")&&i+3<argc) {
       for (j=0;j<3;j++) pos[j]=atof(argv[++i]); /* lat,lon,hgt */
     }
+    else if (!strcmp(argv[i],"-sys")&&i+1<argc) {
+      char *p;
+      for (p=argv[++i];*p;p++) {
+        switch (*p) {
+          case 'G': rnxopt.navsys|=SYS_GPS; break;
+          case 'R': rnxopt.navsys|=SYS_GLO; break;
+          case 'E': rnxopt.navsys|=SYS_GAL; break;
+          case 'J': rnxopt.navsys|=SYS_QZS; break;
+          case 'C': rnxopt.navsys|=SYS_CMP; break;
+          case 'I': rnxopt.navsys|=SYS_IRN; break;
+          case 'S': rnxopt.navsys|=SYS_SBS; break;
+          default : {
+            fprintf(stderr,"invalid system %c\n",*p);
+            return -1;
+          };
+        };
+        if (!(p=strchr(p,','))) break;
+      };
+    }
     else {
       infile[n++]=argv[i];
-    }
-  }
+    };
+  };
+
   if (n<=0) {
     fprintf(stderr,"no input file\n");
     return -1;
-  }
+  };
   if (!*outfile) {
     fprintf(stderr,"no output file\n");
     return -1;
-  }
+  };
   if (norm(pos,3)<=0.0) {
     fprintf(stderr,"no receiver pos\n");
     return -1;
-  }
+  };
+
+
   pos[0]*=D2R; pos[1]*=D2R; pos2ecef(pos,rr);
+
+  /* Default navigation systems */
+  if (!rnxopt.navsys) {
+    rnxopt.navsys=SYS_GPS|SYS_GLO;
+  };
 
   /* read simulated/real rinex nav files */
   for (i=0; i<n; i++) {
@@ -286,15 +328,16 @@ int main(int argc, char **argv)
   if (nav.n<=0) {
     fprintf(stderr,"no nav data\n");
     return -1;
-  }
+  };
+
   /* generate simulated observation data */
-  if (!simobs(ts,te,tint,rr,&nav,&obs,opt)) return -1;
+  if (!simobs(ts,te,tint,rr,&nav,&obs)) return -1;
 
   /* output rinex obs file */
   if (!(fp=fopen(outfile,"w"))) {
     fprintf(stderr,"error : outfile open %s\n",outfile);
     return -1;
-  }
+  };
   fprintf(stderr,"saving...: %s\n",outfile);
   strcpy(rnxopt.prog,PROGNAME);
   strcpy(rnxopt.comment[0],"SIMULATED OBS DATA");
@@ -302,7 +345,6 @@ int main(int argc, char **argv)
   rnxopt.tstart=ts;
   rnxopt.tend=te;
   rnxopt.tint=tint;
-  rnxopt.navsys=SYS_ALL;
   rnxopt.obstype=OBSTYPE_PR|OBSTYPE_CP|OBSTYPE_SNR;
   rnxopt.freqtype=FREQTYPE_L1|FREQTYPE_L2|FREQTYPE_L3|FREQTYPE_L4;
   for (i=0;i<3;i++) rnxopt.apppos[i]=rr[i];
@@ -310,13 +352,14 @@ int main(int argc, char **argv)
   uint8_t codes[7][33]={{0}};
 
   const char type_str[]="CLS";
-  char type[16],*id;
+  char type[16];
 
   int iSys,iObs,iTyp;
   int nSys,nObs,nTyp;
 
   nObs = 4;
   nTyp = 3;
+  nSys = 7;
 
   /* GPS */
 
@@ -329,7 +372,7 @@ int main(int argc, char **argv)
 
   codes[1][0]=CODE_L1C;
   codes[1][1]=CODE_L1P;
-  codes[1][2]=CODE_L1C;
+  codes[1][2]=CODE_L2C;
   codes[1][3]=CODE_L2P;
 
   /* Galileo */
@@ -339,7 +382,7 @@ int main(int argc, char **argv)
   codes[2][2]=CODE_L7Q;
   codes[2][3]=CODE_L6C;
 
-  for (iSys=0;iSys<7;iSys++) {
+  for (iSys=0;iSys<nSys;iSys++) {
 
     rnxopt.nobs[iSys] = (iSys<3? nObs*nTyp : 0);
     if (iSys>2) continue;
@@ -348,12 +391,12 @@ int main(int argc, char **argv)
 
     for (iObs=0;iObs<nObs;iObs++) {
       for (iTyp=0;iTyp<nTyp;iTyp++) {
-				sprintf(type,"%c%s",type_str[iTyp],code2obs(codes[iSys][iObs]));
-				fprintf(stdout," %s",type);
-				setstr(rnxopt.tobs[iSys][n++],type,3);
+        sprintf(type,"%c%s",type_str[iTyp],code2obs(codes[iSys][iObs]));
+        setstr(rnxopt.tobs[iSys][n++],type,3);
       }
     }
-  }
+
+  };
 
   outrnxobsh(fp,&rnxopt,&nav);
 
